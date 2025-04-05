c07edd0eda354334e86a38da859fa9b0da616efa
Cobalt6700 <44681021+Cobalt6700@users.noreply.github.com>
2025-04-05 16:44:00 +0100

    Update MoToMegaAVR.cpp

--- src/megaAVR/MoToMegaAVR.cpp
+++ src/megaAVR/MoToMegaAVR.cpp
@@ -8,6 +8,7 @@
 
 #warning "TIMER HW specfic - megaavr ( Mega4809 ) ---"
 
+
 uint8_t noStepISR_Cnt;   // Counter for nested StepISr-disable
 
 nextCycle_t nextCycle;
@@ -128,6 +129,9 @@ void enableSoftLedIsrAS() {
 #include <utilities/MoToDbg.h>
 
 #warning "TIMER HW specfic - megatinycore ---"
+#ifndef MILLIS_USE_TIMERB1
+  #error "This sketch is written for use with TCB2 as the millis timing source"
+#endif
 
 uint8_t noStepISR_Cnt;   // Counter for nested StepISr-disable
 
@@ -197,24 +201,25 @@ void seizeTimerAS() {
 		// CMP2	register (not yet )used for  softleds
         noInterrupts();
         takeOverTCA0(); 
-        // TCA0_SINGLE_CTRLESET = TCA_SINGLE_CMD_RESET_gc;     // hard reset timer // Not reuired because of takeOverTCA0
-        TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV8_gc;      // 0,5µs per tic, timer disabled - not possible because it influences millis()
-        // TCA0_SINGLE_CTRLA = TCA_SINGLE_CLKSEL_DIV64_gc;      // 4µs per tic ( also used by millis() ), timer disabled
-		//+-> Arduino default of prescaler TCA0 is 64, changes would influence millis()
+        // TCA0.SINGLE.CTRLESET = TCA_SINGLE_CMD_RESET_gc;     // hard reset timer // Not rqeuired because of takeOverTCA0()
+        TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV8_gc;      // 0,5µs per tic, timer disabled - not possible on megaavr because it influences millis()
+        // TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV64_gc;      // 4µs per tic ( also used by millis() ), timer disabled
+		//+-> megaavr Arduino default of prescaler TCA0 is 64, changes would influence millis()
 		// this means 4µs per tic for TCA0
+        // meagtinycore allows a prescaler of 2 for TCB1 for millis(), so changing TCA0 should not influence millis()
         TCA0.SINGLE.CTRLB = TCA_SINGLE_WGMODE_NORMAL_gc;    // normal mode, no autolockUpdate, no pins active
         TCA0.SINGLE.CTRLC = 0;
         TCA0.SINGLE.CTRLD = 0;                              // no split mode ( user 16bit timer )
-        //TCA0_SINGLE_CTRLECLR
+        //TCA0.SINGLE.CTRLECLR                              // wasn't used in megaavr code - not sure what it would do here
         TCA0.SINGLE.CTRLESET = TCA_SINGLE_LUPD_bm;          // don't use buffered compare registes
-        //TCA0_SINGLE_CTRLFCLR
-        //TCA0_SINGLE_CTRLFSET
-        //TCA0_SINGLE_INTCTRL = TCA_SINGLE_CMP0_bm | TCA_SINGLE_CMP1_bm; // enable cmp0 and cmp1 interrupt
-        TCA0.SINGL.INTFLAGS = 0;   // clear all interrupts
+        //TCA0.SINGLE.CTRLFCLR                              / wasn't used in megaavr code - not sure what it would do here
+        //TCA0.SINGLE.CTRLFSET                              // wasn't used in megaavr code - not sure what it would do here              
+        TCA0_SINGLE.INTCTRL = TCA_SINGLE_CMP0_bm | TCA_SINGLE_CMP1_bm; // enable cmp0 and cmp1 interrupt
+        TCA0.SINGLE.INTFLAGS = 0;   // clear all interrupts
         TCA0.SINGLE.PER  = TIMERPERIODE * TICS_PER_MICROSECOND;  // timer periode is 20000us 
         TCA0.SINGLE.CMP0 = FIRST_PULSE;   
         TCA0.SINGLE.CMP1 = 400;  
-        TCA0.SINGL.CTRLA |= TCA_SINGLE_ENABLE_bm;          // Enable the timer
+        TCA0.SINGLE.CTRLA |= TCA_SINGLE_ENABLE_bm;          // Enable the timer
         interrupts();
         timerInitialized = true;  
         MODE_TP1;   // set debug-pins to Output
