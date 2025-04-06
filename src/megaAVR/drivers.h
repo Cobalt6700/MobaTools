5a21b3bb03696f50b19ce7c14e7eeb02180a3ce2
Cobalt6700 <44681021+Cobalt6700@users.noreply.github.com>
2025-04-06 14:04:26 +0100

    Update drivers.h

--- src/megaAVR/drivers.h
+++ src/megaAVR/drivers.h
@@ -3,10 +3,10 @@
 //////////////////////////////////////// processor dependent defines and declarations //////////////////////////////////////////
 //vvvvvvvvvvvvvvvvvvvvvvvvvvvvv  megaAVR ATMega4809 (Nano Every) vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 /* MobaTools uses Timer/Counter 0 (TCA0) on NanoEvery. Becuase the prescaler of TCA0 is also used for millis() on Timer TCB3,
-   it must not be changed. This leads to 4µs per tic. This is no problem for stepper and Softleds, but the resolution of servos is decreased compared to other AVR processors.   
+   it must not be changed. This leads to 4µs per tic. This is no problem for stepper and Softleds, but the resolution of servos is decreased compared to other AVR processors.
    This would be better with megaCoreX boardmanager ( it doesn't use TCA0 prescaler for millis() ), but this boardmanager doesn't support the standard bootloader of Nano Every.
-   
-*/   
+
+*/
 //#include <avr/interrupt.h>
 #define IRAM_ATTR       // delete in .cpp files, because it has no meaning for megaAVR processors
 #define DRAM_ATTR
@@ -14,8 +14,14 @@
 
 #define FAST_PORTWRT        // if this is defined, ports are written directly in IRQ-Routines,
                             // not with 'digitalWrite' functions
-//#define TICS_PER_MICROSECOND (clockCyclesPerMicrosecond() / 64.0) // prescaler is 64 = 4µs per tic (used by millis() too )
+// For megaAVR (Nano Every) - prescaler is 64 = 4µs per tic (used by millis() too)
+#if defined(MEGATINYCORE) && defined(MILLIS_USE_TIMERB1)
+// For ATtiny with megaTinyCore - can use prescaler of 8 = 0.5µs per tic
+#define TICS_PER_MICROSECOND (F_CPU / 1000000.0 / 8) // the clockCyclesPerMicrosecond() call inhibits compiletime evaluation
+#else
+// For standard megaAVR - must use prescaler of 64 to not interfere with millis()
 #define TICS_PER_MICROSECOND (F_CPU / 1000000.0 / 64) // the clockCyclesPerMicrosecond() call inhibits compiletime evaluation
+#endif
 
 // define timer to use
 // defines specially für Nano Every ( MegaAVR4808 ).
@@ -31,7 +37,7 @@
 #define TIMERx_COMPB_vect   TCA0_CMP1_vect
 #define TIMERx_COMPC_vect   TCA0_CMP2_vect
 #define TIMSKx     TCA0_SINGLE_INTCTRL
-#define OCIExB	
+#define OCIExB
 
 
 #define ARCHITECT_INCLUDE <megaAVR/MoToMegaAVR.h>
