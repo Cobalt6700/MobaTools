05715059d67dc622c03957c29b750369852525df
Cobalt6700 <44681021+Cobalt6700@users.noreply.github.com>
2025-04-14 16:36:51 +0100

    Add megaTinyAVR code

---
+++ src/megaTinyAVR/driversMegaTinyAVR.c
@@ -0,0 +1,7 @@
+// AVR HW-spcific Functions
+#if defined(ARDUINO_ARCH_MEGAAVR) && defined (MEGATINYCORE)
+#include <Arduino.h>
+#include "drivers.h"
+
+#warning "HW specfic - megatinyavr ---"
+#endif
